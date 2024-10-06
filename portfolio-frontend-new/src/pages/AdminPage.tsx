0f4ba10eab36dd0d0bc3fbfbb5f4573728358863
HashiruG <hashiruhgunathilake@gmail.com>
2024-10-06 14:30:42 +0530

    Setting up the admin page ui

--- portfolio-frontend-new/src/pages/AdminPage.tsx
+++ portfolio-frontend-new/src/pages/AdminPage.tsx
@@ -1,18 +1,80 @@
-import NavigationCustomized from "@/components/ui/navigation-customized"
-
+import React from "react";
+import NavigationCustomized from "@/components/ui/navigation-customized";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const AdminPage = () => {
-    return (
-        <div>
-          <NavigationCustomized
-          logo="Hashiru"
-          navItems={[
-            { href: "#about", description: "Add projects" },
-            { href: "#skills", description: "Add Skills" }
-          ]}
-        ></NavigationCustomized>
-        </div>
-    )
-}
+  return (
+    <>
+      <NavigationCustomized
+        logo="Hashiru"
+        navItems={[{ href: "", description: "Admin Panel" }]}
+      />
+      <div className="flex flex-col p-36 gap-10">
+        <Card>
+          <CardHeader>
+            <CardTitle>Add Projects</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <Label htmlFor="title">Project Title:</Label>
+              <Input
+                className="my-2"
+                id="title"
+                type="text"
+                placeholder="Enter Project Title"
+              />
 
-export default AdminPage
+              <Label htmlFor="description">Project Description:</Label>
+              <Textarea
+                className="my-2"
+                id="description"
+                placeholder="Enter Project description"
+              />
+
+              <Label htmlFor="img">Project Image:</Label>
+              <Input className="my-2" id="img" type="file"></Input>
+              <Button className="my-2" type="submit">
+                Submit
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Add Skills</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <Label htmlFor="name">Skill Name:</Label>
+              <Input
+                className="my-2"
+                id="name"
+                type="text"
+                placeholder="Enter Skill Name"
+              />
+
+              <Label htmlFor="url">Skill Icon URL:</Label>
+              <Input
+                className="my-2"
+                id="url"
+                type="text"
+                placeholder="Enter Icon URL"
+              />
+
+              <Button className="my-2" type="submit">
+                Submit
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
