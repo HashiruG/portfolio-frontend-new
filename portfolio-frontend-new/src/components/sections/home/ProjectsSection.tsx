115102ede1bc3ab47e523df92ba771401273be7f
HashiruG <hashiruhgunathilake@gmail.com>
2024-10-08 14:04:34 +0530

    Fetch skills from backend in skillsSection

--- portfolio-frontend-new/src/components/sections/home/ProjectsSection.tsx
+++ portfolio-frontend-new/src/components/sections/home/ProjectsSection.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import {
   Carousel,
--- portfolio-frontend-new/src/components/sections/home/SkillsSection.tsx
+++ portfolio-frontend-new/src/components/sections/home/SkillsSection.tsx
@@ -1,11 +1,51 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card} from "@/components/ui/card";
-
+import { Card } from "@/components/ui/card";
+import { useState, useEffect } from "react";
 
 const SkillsSection = () => {
-    return(
-        <>
-            <section
+  const [programmingSkills, setProgrammingSkills] = useState([]);
+  const [webSkills, setWebSkills] = useState([]);
+  const [mlSkills, setMlSkills] = useState([]);
+
+  const fetchProgrammingSkills = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/programming_skills");
+      const data = await response.json();
+      setProgrammingSkills(data);
+    } catch (error) {
+      console.error("Error fetching programming skills:", error);
+    }
+  };
+
+  const fetchWebSkills = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/web");
+      const data = await response.json();
+      setWebSkills(data);
+    } catch (error) {
+      console.error("Error fetching web skills:", error);
+    }
+  };
+
+  const fetchMlSkills = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/ml");
+      const data = await response.json();
+      setMlSkills(data);
+    } catch (error) {
+      console.error("Error fetching ML skills:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProgrammingSkills();
+    fetchWebSkills();
+    fetchMlSkills();
+  }, []);
+
+  return (
+    <>
+      <section
         id="skills"
         className="flex flex-col items-center justify-center h-auto min-h-screen"
       >
@@ -38,111 +78,44 @@ const SkillsSection = () => {
 
             <TabsContent className="mt-14 md:mt-10" value="programming">
               <Card className="w-full flex flex-wrap p-4 gap-10 md:gap-12 py-6 justify-center">
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg"
-                />
+                {programmingSkills.map((skill, index) => (
+                  <img
+                    key={index}
+                    className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
+                    src={skill.skillURL}
+                  />
+                ))}
               </Card>
             </TabsContent>
 
             <TabsContent className="mt-14 md:mt-10" value="webdev">
               <Card className="w-full flex flex-wrap p-4 gap-10 md:gap-12 py-6 justify-center">
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-plain-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg"
-                />
+                {webSkills.map((skill, index) => (
+                  <img
+                    key={index}
+                    className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
+                    src={skill.skillURL}
+                  />
+                ))}
               </Card>
             </TabsContent>
 
             <TabsContent className="mt-14 md:mt-10" value="ml">
               <Card className="w-full flex flex-wrap p-4 gap-10 md:gap-12 py-6 justify-center">
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original-wordmark.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg"
-                />
-                <img
-                  className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original-wordmark.svg"
-                />
+                {mlSkills.map((skill, index) => (
+                  <img
+                    key={index}
+                    className="h-[5rem] w-[5rem] md:h-[6rem] md:w-[6rem] my-3 md:my-4"
+                    src={skill.skillURL}
+                  />
+                ))}
               </Card>
             </TabsContent>
           </Tabs>
         </div>
       </section>
-        </>
-    )
-}
+    </>
+  );
+};
 
-export default SkillsSection
+export default SkillsSection;
